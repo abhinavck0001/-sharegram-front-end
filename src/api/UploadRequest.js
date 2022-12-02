ed349eee52b96b3a00563ed6e6aa3e160e41e0f1
abhinav ck <abhinavck0001@gmail.com>
2022-12-03 00:36:05 +0530

    added ip instead

--- src/api/UploadRequest.js
+++ src/api/UploadRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API = axios.create({baseURL: "http://localhost:5000"})
+const API = axios.create({baseURL: "http://3.108.190.210"})
 
 export const uploadImage = (data) => API.post('/upload',data)
 
--- src/api/messageRequest.js
+++ src/api/messageRequest.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({baseURL: "http://localhost:5000"})
+const API = axios.create({baseURL: "http://3.108.190.210"})
 
 export const getMessages =(id) => API.get(`/message/${id}`)
 export const addMessage =(data) => API.post('/message',data)
