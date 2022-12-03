bfa7f4e24a85444628261adec6467f3edb1d11f4
abhinav ck <abhinavck0001@gmail.com>
2022-12-03 12:26:03 +0530

    bugs fixed

--- src/components/profileSide/ProfileSide.jsx
+++ src/components/profileSide/ProfileSide.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import FollowersCard from '../followersCard/FollowersCard'
-import LogoSearch from '../logosearch/LogoSearch'
+// import LogoSearch from '../logosearch/LogoSearch'
 import ProfileCard from '../profileCard/ProfileCard'
 import './ProfileSide.css'
 
 const ProfileSide = () => {
   return (
     <div className="ProfileSide">
-        <LogoSearch/>
+        {/* <LogoSearch/> */}
         <ProfileCard location="homepage"/>
         <FollowersCard/>
     </div>
